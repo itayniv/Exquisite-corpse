d89e62c674ca4b93fc8253432648f6b8126508d7
Itay Niv <itayniv@gmail.com>
2018-11-03 23:29:01 -0400

    hit_Detection

---
+++ Assets/grabObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabObject : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("starting");
+    }
+
+
+    void Update()
+    {
+        //Debug.Log(transform.position);
+    }
+
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("boooom");
+    }
+
+}
